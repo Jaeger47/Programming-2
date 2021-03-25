861eb984690fa7cd6abffad943a7c581749e2e0b
Mark Daniel G. Dacer <markdacer21@gmail.com>
2021-03-25 08:37:08 +0800

    Update Practical Act 1 - Source Code.c

--- Practical Act 1 - Source Code.c
+++ Practical Act 1 - Source Code.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
+//function prototypes
 void add_info(int id_func, int age_func, char sex_func); 
 void vote_test(int age_func);
 
 int main()
 {
+    //local Variables
     int age,id_number;
     char sex;
     
+    //get user info
     printf("Input your Sex: ");
     scanf("%c", &sex);
     printf("Input your ID Number: ");
@@ -15,8 +18,9 @@ int main()
     printf("Input your Age: ");
     scanf("%d", &age);
     
-    
+    //pass arg on id,age,sex
     add_info(id_number,age,sex);
+    //pass arg on age to test
     vote_test(age);
     
     
@@ -25,12 +29,14 @@ int main()
 
 void add_info(int id_func, int age_func, char sex_func)
 {
+    //print all user info
     printf("_______________________________________________________\n");
     printf("Sex: %c\nID#: %d\nAge: %d\n",sex_func,id_func,age_func);
 }
 
 void vote_test(int age_func)
 {
+    //check age for eligiblity 
     if(age_func < 18)
     {
         printf("You not eligible for Voting");
